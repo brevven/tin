2fa6eb74bbe5852d6128693ac08d5644e8360df7
Brevven <brevingame@gmail.com>
2022-07-18 21:31:01 -0700

    stacking bronze

--- tin-recipe-final-stacking.lua
+++ tin-recipe-final-stacking.lua
@@ -9,6 +9,9 @@ if deadlock then
   if data.raw.item["tinned-cable"] then
     deadlock.add_stack("tinned-cable", nil , "deadlock-stacking-2", nil)
   end
+  if data.raw.item["bronze-plate"] then
+    deadlock.add_stack("bronze-plate", nil , "deadlock-stacking-1", nil)
+  end
   if mods.Krastorio2 then
     deadlock.add_stack("enriched-tin", "__bztin__/graphics/icons/stacked/enriched-tin-ore-stacked.png" , "deadlock-stacking-3", 128)
   end
@@ -21,6 +24,9 @@ if deadlock_crating then
   deadlock_crating.add_crate("solder", "deadlock-crating-1")
   if data.raw.item["tinned-cable"] then
     deadlock_crating.add_crate("tinned-cable", "deadlock-crating-2")
+  end
+  if data.raw.item["bronze-plate"] then
+    deadlock_crating.add_crate("bronze-plate", "deadlock-crating-1")
   end
   if mods.Krastorio2 then
     deadlock_crating.add_crate("enriched-tin", "deadlock-crating-3")
