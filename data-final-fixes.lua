5fd5aa4728d1cd4fbdae50df669b4b53d881f8fa
Brevven <brevingame@gmail.com>
2023-03-28 03:16:14 -0700

    noop fix recipe time

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -15,12 +15,14 @@ util.add_to_product("se-core-fragment-omni", "tin-ore", -4)
 local amt_ec = util.get_amount("electronic-circuit-stone", "electronic-circuit")
 if amt_ec == 1 then
   util.multiply_recipe("electronic-circuit-stone", 2)
+  util.set_recipe_time("electronic-circuit-stone", 0.5) -- faster but more complex
 end
 util.replace_some_ingredient("electronic-circuit-stone", "copper-cable", 1, "solder", 1)
 
 amt_ec = util.get_amount("electronic-circuit")
 if amt_ec == 1 then
   util.multiply_recipe("electronic-circuit", 2)
+  util.set_recipe_time("electronic-circuit-stone", 0.5) -- faster but more complex
 end
 util.replace_some_ingredient("electronic-circuit", "copper-cable", 1, "solder", 1)
 
