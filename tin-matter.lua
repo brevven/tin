cb3d3834a66f4d2714c8cc24c3ab8fe880e43622
Brevven <brevingame@gmail.com>
2023-03-28 18:51:44 -0700

    matter update

--- tin-matter.lua
+++ tin-matter.lua
@@ -1,60 +1,27 @@
 -- Matter recipes for Krastorio2
 if mods["Krastorio2"] then
-local util = require("data-util");
-local matter = require("__Krastorio2__/lib/public/data-stages/matter-util")
-
-data:extend(
-{
-  {
-    type = "technology",
-    name = "tin-matter-processing",
-    icons =
-    {
-      {
-        icon = util.k2assets().."/technologies/matter-coal.png",
-        icon_size = 256,
-      },
-      {
-        icon = "__bztin__/graphics/icons/tin-ore.png",
-        icon_size = 64,
-        scale = 1.4,
-      }
+  local util = require("data-util");
+  util.k2matter({
+    k2matter = {
+      item_name = "tin-ore",
+      matter_value = 5,
+      energy_required = 1,
+      need_stabilizer = false,
+      unlocked_by_technology = "tin-matter-processing",
     },
-    prerequisites = {"kr-matter-processing"},
-    unit =
-  	{
-      count = 350,
-      ingredients =
-      {
-        {"production-science-pack", 1},
-        {"utility-science-pack", 1},
-        {"matter-tech-card", 1}
-      },
-      time = 45
-    }
-  },
-})
+    k2baseicon = "stone",
+    icon = { icon = "__bztin__/graphics/icons/tin-ore.png", icon_size = 64, scale = 1.4}
+  })
 
-local tin_ore_matter = 
-	{
-    item_name = "tin-ore",
-    minimum_conversion_quantity = 10,
-    matter_value = 5,
-    energy_required = 1,
-    need_stabilizer = false,
-    unlocked_by_technology = "tin-matter-processing"
-	}
-matter.createMatterRecipe(tin_ore_matter)
-
-local tin_plate_matter = 
-	{
-    item_name = "tin-plate",
-    minimum_conversion_quantity = 10,
-    matter_value = 10,
-    energy_required = 3,
-    only_deconversion = true,
-    need_stabilizer = true,
-    unlocked_by_technology = "tin-matter-processing"
-	}
-matter.createMatterRecipe(tin_plate_matter)
+  util.k2matter({
+    k2matter = {
+      item_name = "tin-plate",
+      minimum_conversion_quantity = 10,
+      matter_value = 10,
+      energy_required = 3,
+      only_deconversion = true,
+      need_stabilizer = true,
+      unlocked_by_technology = "tin-matter-processing",
+    },
+  })
 end
